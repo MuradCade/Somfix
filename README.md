e86ea68d271b55e6c38f1358c91e4119ced1f208
MuradCade <king12murad@gmail.com>
2023-03-03 11:05:09 -0800

    readme file added

---
+++ README.md
@@ -0,0 +1,8 @@
+# Somfix(Online-Repiar-Service-Provider)
+
+### This project is build using Following Technologies
+     - Flutter
+     - Dart
+     - Firebase (Backend as service)
+
+#####  This project is online repair service that means this app provides the ability to order repair serices from any where.
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Mainapp(),
+    );
+  }
+}
+
+class Mainapp extends StatefulWidget {
+  const Mainapp({super.key});
+
+  @override
+  State<Mainapp> createState() => _MainappState();
+}
+
+bool _iconbool = false;
+IconData _iconlight = Icons.wb_sunny;
+IconData _icondark = Icons.nights_stay;
+
+// light theme
+ThemeData _lightTheme =
+    ThemeData(primaryColor: Colors.amber, brightness: Brightness.light);
+
+// dark theme
+ThemeData _darktheme =
+    ThemeData(primaryColor: Colors.red, brightness: Brightness.dark);
+
+class _MainappState extends State<Mainapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: _iconbool ? _darktheme : _lightTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    _iconbool = !_iconbool;
+                  });
+                },
+                icon: Icon(
+                  _iconbool ? _icondark : _iconlight,
+                  color: Colors.orange,
+                )),
+          ],
+        ),
+        body: Column(children: []),
+      ),
+    );
+  }
+}
